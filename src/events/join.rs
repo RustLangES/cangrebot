14af1f10a6b84a56dec8bc2550d37f39df40c88c
crawkatt <man849916@gmail.com>
2024-03-21 11:57:37 -0300

    ✨ feat: A new feature has been added to the Bot welcomes. Now the Bot sends an image with the avatar as a welcome along with a message

--- src/events/join.rs
+++ src/events/join.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use serenity::{model::prelude::*, prelude::*};
 use std::convert::TryFrom;
-
+use plantita_welcomes::create_welcome::combine_images;
 
 pub async fn guild_member_addition(ctx: &Context, guild_id: &GuildId, member: &Member) {
     if let Err(e) = _guild_member_addition(ctx, guild_id, member).await {
@@ -23,7 +23,24 @@ async fn _guild_member_addition(ctx: &Context, guild_id: &GuildId, member: &Memb
             &guild_id.name(ctx).unwrap_or_else(|| "".into()),
         );
 
-    let msg = msg_channel.say(&ctx, join_msg_replaced).await?;
+    let avatar_url = member.user.avatar_url().unwrap_or_else(|| member.user.default_avatar_url());
+    let response = reqwest::get(avatar_url).await?;
+    let bytes = response.bytes().await?;
+
+    let img = image::load_from_memory(&bytes)?;
+    img.resize(256, 256, image::imageops::Lanczos3);
+    img.save_with_format(format!("/tmp/{}_avatar.png", member.user.name), image::ImageFormat::Png)?;
+
+    let avatar_path = format!("/tmp/{}_avatar.png", member.user.name);
+    let output_path = format!("/tmp/{}_welcome.png", member.user.name);
+    combine_images("./static/background.png", &avatar_path, 74, 74, 372, &output_path)?;
+
+    let msg = msg_channel.send_files(&ctx, vec![output_path.as_str()], |m| {
+        m.content(&join_msg_replaced)
+    }).await?;
+
+    std::fs::remove_file(avatar_path)?;
+    std::fs::remove_file(output_path)?;
 
     // Convert string emoji to ReactionType to allow custom emojis
     let reaction = ReactionType::try_from("👋")?;
