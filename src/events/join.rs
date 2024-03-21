8a2077c2f056164b0f3368e975daddbeb29d4e63
crawkatt <man849916@gmail.com>
2024-03-21 14:56:15 -0300

    ⚡️ improve: Improving the welcome system

--- src/events/join.rs
+++ src/events/join.rs
@@ -13,7 +13,7 @@ pub async fn guild_member_addition(ctx: &Context, guild_id: &GuildId, member: &M
 async fn _guild_member_addition(ctx: &Context, guild_id: &GuildId, member: &Member) -> Result<()> {
     let join_msg = "Bienvenid@ <mention> a <server>! Pásala lindo!".to_string();
 
-    let msg_channel = ChannelId(778674893851983932_u64);
+    let msg_channel = ChannelId(1211486599322542150_u64);
 
     let join_msg_replaced = join_msg
         .replace("<mention>", &member.user.mention().to_string())
@@ -23,24 +23,25 @@ async fn _guild_member_addition(ctx: &Context, guild_id: &GuildId, member: &Memb
             &guild_id.name(ctx).unwrap_or_else(|| "".into()),
         );
 
+    // Download the user's avatar and create a welcome image
     let avatar_url = member.user.avatar_url().unwrap_or_else(|| member.user.default_avatar_url());
     let response = reqwest::get(avatar_url).await?;
     let bytes = response.bytes().await?;
 
     let img = image::load_from_memory(&bytes)?;
     img.resize(256, 256, image::imageops::Lanczos3);
-    img.save_with_format(format!("/tmp/{}_avatar.png", member.user.name), image::ImageFormat::Png)?;
+    let mut background = image::open("./static/background.png")?;
 
-    let avatar_path = format!("/tmp/{}_avatar.png", member.user.name);
     let output_path = format!("/tmp/{}_welcome.png", member.user.name);
-    combine_images("./static/background.png", &avatar_path, 74, 74, 372, &output_path)?;
+    combine_images(&mut background, &img, 74, 74, 372)?;
+    background.save(output_path.as_str())?;
 
     let msg = msg_channel.send_files(&ctx, vec![output_path.as_str()], |m| {
         m.content(&join_msg_replaced)
     }).await?;
 
-    std::fs::remove_file(avatar_path)?;
-    std::fs::remove_file(output_path)?;
+    // Remove the file after sending the message
+    std::fs::remove_file(&output_path)?;
 
     // Convert string emoji to ReactionType to allow custom emojis
     let reaction = ReactionType::try_from("👋")?;
