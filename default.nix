be4d24704350f8a826d54869589e75c42a8a46d6
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-03-31 18:14:18 -0400

    chore: add nix develop shell

---
+++ default.nix
@@ -0,0 +1,55 @@
+let
+  inherit
+    (builtins)
+    currentSystem
+    fromJSON
+    readFile
+    ;
+  getFlake = name:
+    with (fromJSON (readFile ./flake.lock)).nodes.${name}.locked; {
+      inherit rev;
+      outPath = fetchTarball {
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        sha256 = narHash;
+      };
+    };
+in
+  {
+    system ? currentSystem,
+    pkgs ? import (getFlake "nixpkgs") {localSystem = {inherit system;};},
+    lib ? pkgs.lib,
+    crane,
+    cranix,
+    fenix,
+    ...
+  }: let
+    # fenix: rustup replacement for reproducible builds
+    toolchain = fenix.${system}.fromToolchainFile {
+      file = ./rust-toolchain.toml;
+      sha256 = "sha256-e4mlaJehWBymYxJGgnbuCObVlqMlQSilZ8FljG9zPHY=";
+    };
+    # crane: cargo and artifacts manager
+    craneLib = crane.${system}.overrideToolchain toolchain;
+    # cranix: extends crane building system with workspace bin building and Mold + Cranelift integrations
+    cranixLib = craneLib.overrideScope' (cranix.${system}.craneOverride);
+
+    # buildInputs for Examples
+    buildInputs = with pkgs; [
+      openssl
+      libopus
+    ];
+  in {
+    # `nix develop`
+    devShells.default = cranixLib.devShell {
+      depsBuildBuild = [ pkgs.stdenv.cc ];
+      packages = with pkgs;
+        [
+          cmake
+          toolchain
+          pkg-config
+          cargo-shuttle
+        ]
+        ++ buildInputs;
+      LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+    };
+  }
