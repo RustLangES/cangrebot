cf209bb04f077dca741c109217149ac1266698cf
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-04-01 01:55:23 -0400

    fix: add tag from name

--- src/events/daily_challenge.rs
+++ src/events/daily_challenge.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serenity::all::ForumTagId;
-use serenity::builder::{CreateForumPost, CreateMessage};
+use serenity::builder::{CreateForumPost, CreateForumTag, CreateMessage};
 use serenity::model::prelude::ChannelId;
 use serenity::prelude::Context;
 use serenity::Result;
@@ -10,7 +10,7 @@ use tracing::info;
 pub struct DailyChallengeRequest {
     title: String,
     message: String,
-    tag: u64,
+    tag_name: String,
 }
 
 pub async fn run_daily_challenge(
@@ -18,17 +18,22 @@ pub async fn run_daily_challenge(
     DailyChallengeRequest {
         title,
         message,
-        tag,
+        tag_name,
     }: &DailyChallengeRequest,
 ) -> Result<()> {
     info!("Running create suggestion");
     let msg_channel = ChannelId::new(824695624665923594_u64.into());
 
+    let forum = msg_channel.to_channel(ctx).await?.guild().ok_or(serenity::Error::Other("GuildId not found"))?;
+    let Some(tag) = forum.available_tags.iter().find(|t| &t.name == tag_name) else {
+        return Err(serenity::Error::Other("Tag not found"));
+    };
+
     let _ = msg_channel
         .create_forum_post(
             &ctx,
             CreateForumPost::new(title, CreateMessage::new().content(message))
-                .add_applied_tag(ForumTagId::new(*tag)),
+                .add_applied_tag(tag.id),
         )
         .await?;
 
