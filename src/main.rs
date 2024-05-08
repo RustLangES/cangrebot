0cc0c721badef920bbe48e9b9a8a294b3e7722ad
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-08 02:55:28 -0400

    fix: use secrets instead of environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::extract::Request;
+use axum::extract::{Request, State};
 use axum::http::{HeaderMap, StatusCode};
 use axum::middleware::Next;
 use axum::response::Response;
@@ -18,14 +18,11 @@ pub mod events;
 pub mod general_commands;
 pub mod slash_commands;
 use config::setup::setup;
-use once_cell::sync::Lazy;
 
-#[macro_use]
-extern crate litcrypt2;
-
-use_litcrypt!();
-
-static BOT_API_KEY: Lazy<String> = Lazy::new(|| lc!(env!("BOT_APIKEY")));
+#[derive(Clone, Debug)]
+struct SecretsState {
+    bot_apikey: String,
+}
 
 pub struct CustomService {
     discord_bot: Client,
@@ -53,22 +50,34 @@ impl shuttle_runtime::Service for CustomService {
     }
 }
 
-async fn auth_token(headers: HeaderMap, req: Request, next: Next) -> Result<Response, StatusCode> {
+async fn auth_token(
+    State(secrets): State<SecretsState>,
+    headers: HeaderMap,
+    req: Request,
+    next: Next,
+) -> Result<Response, StatusCode> {
     if headers
         .get("Authorization")
         .as_ref()
-        .is_some_and(|k| k.to_str().unwrap() == BOT_API_KEY.as_str())
+        .is_some_and(|k| k.to_str().unwrap() == &secrets.bot_apikey)
     {
         return Ok(next.run(req).await);
     }
     Err(axum::http::StatusCode::UNAUTHORIZED)
 }
 
-fn build_router(ctx: Arc<Http>) -> Router {
+fn build_router(secrets: SecretStore, ctx: Arc<Http>) -> Router {
     Router::new()
         .route("/daily_challenge", axum::routing::post(run_daily_challenge))
         .route("/send_message", axum::routing::post(send_message))
-        .layer(middleware::from_fn(auth_token))
+        .layer(middleware::from_fn_with_state(
+            SecretsState {
+                bot_apikey: secrets
+                    .get("BOT_APIKEY")
+                    .expect("Cannot get bot apikey to authorize"),
+            },
+            auth_token,
+        ))
         .with_state(ctx)
 }
 
@@ -81,8 +90,8 @@ async fn init(
     };
     let public_folder = PathBuf::from("static");
 
-    let discord_bot = setup(secret_store, public_folder).await?;
-    let router = build_router(discord_bot.http.clone());
+    let discord_bot = setup(secret_store.clone(), public_folder).await?;
+    let router = build_router(secret_store, discord_bot.http.clone());
 
     Ok(CustomService {
         discord_bot,
