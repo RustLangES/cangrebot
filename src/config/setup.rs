17b0a5fb9cd7b09a36ce3b8001ce946ec4317f6c
stifskere <stifskere@gmail.com>
2024-07-24 20:02:21 +0200

    feat: event

--- src/config/setup.rs
+++ src/config/setup.rs
@@ -12,7 +12,7 @@ use songbird::SerenityInit;
 use tracing::{info, instrument};
 
 use crate::config::songbird_config::SoundStore;
-
+use crate::events::read_github_links::ReadGithubLinkHandler;
 use super::general_command_loader::GENERAL_GROUP;
 
 use super::slash_command_loader::Handler;
@@ -51,6 +51,7 @@ pub async fn setup(secret_store: SecretStore, _: PathBuf) -> Result<Client, anyh
 
     let client = Client::builder(token, intents)
         .event_handler(handler)
+        .event_handler(ReadGithubLinkHandler)
         .framework(framework)
         .register_songbird()
         .await
--- src/events/mod.rs
+++ src/events/mod.rs
@@ -2,3 +2,4 @@ pub mod anti_spam;
 pub mod daily_challenge;
 pub mod join;
 pub mod send_message;
+pub mod read_github_links;
