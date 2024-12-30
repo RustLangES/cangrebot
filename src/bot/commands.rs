9c6d7ff85baf7c88a0460002212e7cde6adb0268
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-12-29 23:57:47 -0400

    feat: get docs from std

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -1,15 +1,16 @@
+mod crate_docs;
 mod explain;
 mod help;
 mod invite;
 mod krate;
 mod ping;
 mod suggest;
-mod crate_docs;
 
 use super::{Data, Error};
 
 pub fn commands() -> Vec<poise::Command<Data, Error>> {
     vec![
+        crate_docs::docs(),
         crate_docs::crate_docs(),
         explain::explain(),
         help::help(),
