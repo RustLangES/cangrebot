4c8bec31feca66636fa42724d592d1ebfa379f8c
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2025-02-10 13:56:43 -0500

    refactor(build): detect submodule folder instead of using env var

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 // dep-installer-hack/build.rs
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=SUBMOD");
-    if std::env::var("SUBMOD").is_ok() {
-        println!("cargo:rerun-if-changed=static/rust-examples");
+    if !std::path::Path::new("static/rust-examples/docs").exists() {
         if !std::process::Command::new("git")
             .args(["submodule", "update", "--init", "--recursive"])
             .status()
