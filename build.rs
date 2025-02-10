06eceb6c7e8aa7c8ae2377f9615ef870d883eedb
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2025-02-09 19:07:23 -0500

    chore: make submodule update optional via SUBMOD=1

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
 // dep-installer-hack/build.rs
 
 fn main() {
-    println!("cargo:rerun-if-changed=static/rust-examples");
-
-    if !std::process::Command::new("git")
-        .args(["submodule", "update", "--init", "--recursive"])
-        .status()
-        .expect("Fallo al ejecutar git submodule update")
-        .success()
-    {
-        panic!("Submodule update failed. Run: git submodule update --init --recursive");
+    println!("cargo:rerun-if-env-changed=SUBMOD");
+    if std::env::var("SUBMOD").is_ok() {
+        println!("cargo:rerun-if-changed=static/rust-examples");
+        if !std::process::Command::new("git")
+            .args(["submodule", "update", "--init", "--recursive"])
+            .status()
+            .expect("Fallo al ejecutar git submodule update")
+            .success()
+        {
+            panic!("Submodule update failed. Run: git submodule update --init --recursive");
+        }
     }
 }
