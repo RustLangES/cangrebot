919eaa90a8d550c1544f3ad5ebddad75f78cc159
Judas9016 <thetrollcrafttnt@gmail.com>
2025-04-02 16:40:00 -0500

    Improvement ping command (#56)
    * Improvement ping command
    
    * cargo format

--- src/bot/commands/ping.rs
+++ src/bot/commands/ping.rs
@@ -1,8 +1,25 @@
 use crate::bot::{Context, Error};
+use poise::{serenity_prelude::CreateEmbed, CreateReply};
+use std::time::Instant;
 
 /// Estoy vivo? Recuerdo el sonido... Ping!... Pong!
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("pong!").await?;
+    let start: Instant = Instant::now();
+
+    ctx.say("Calculating...⌛").await?;
+
+    let latency: u128 = start.elapsed().as_millis();
+
+    let mensaje: String = format!("📡 Latencia: `{}` ms", latency);
+
+    let embed = CreateEmbed::new()
+        .title("🏓 Pong!")
+        .description(mensaje)
+        .color(0xEA9010);
+
+    let replay = CreateReply::default();
+
+    ctx.send(replay.embed(embed)).await?;
     Ok(())
 }
