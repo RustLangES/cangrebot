6f4dd0b9e8006aa564fedd842dabca7ffc3c981b
Larissa ♡⋆｡˚ <97119998+gxskpo@users.noreply.github.com>
2025-08-18 00:23:50 -0600

    fix(tts): ensure stop command halts playback of split messages (#71)

--- src/bot/commands/tts/stop.rs
+++ src/bot/commands/tts/stop.rs
@@ -1,12 +1,11 @@
 use crate::bot;
-use poise::serenity_prelude::{ChannelId, CreateEmbed, UserId};
+use crate::bot::commands::tts::tts::TtsTrackData;
+use poise::serenity_prelude::{ChannelId, CreateEmbed};
 use poise::CreateReply;
 use std::sync::Arc;
-use tracing::info;
 
 #[poise::command(slash_command, prefix_command, guild_only)]
 pub async fn stop(ctx: bot::Context<'_>) -> Result<(), bot::Error> {
-    info!("1");
     let guild_id = ctx.guild().ok_or("No se pudo obtener el guild")?.id;
 
     let manager = songbird::get(ctx.serenity_context())
@@ -27,13 +26,12 @@ pub async fn stop(ctx: bot::Context<'_>) -> Result<(), bot::Error> {
     };
 
     let handler = handler_lock.lock().await;
-    let track_data: Arc<UserId> = handler
+    let track_data: Arc<TtsTrackData> = handler
         .queue()
         .current()
         .ok_or("Failed to get track data")?
         .data();
 
-    let author_id = &*track_data;
     let member = ctx.author_member().await.ok_or("Failed to get member")?;
 
     let guild_channel = ChannelId::new(
@@ -53,7 +51,7 @@ pub async fn stop(ctx: bot::Context<'_>) -> Result<(), bot::Error> {
         .ok_or("Not in a guild")?
         .user_permissions_in(&guild_channel, member.as_ref());
 
-    if author_id != &ctx.author().id && !perms.priority_speaker() {
+    if track_data.author_id != ctx.author().id && !perms.priority_speaker() {
         ctx.send(
             CreateReply::default().embed(
                 CreateEmbed::new()
@@ -69,5 +67,23 @@ pub async fn stop(ctx: bot::Context<'_>) -> Result<(), bot::Error> {
     ctx.send(CreateReply::default().content("⏭ ")).await?;
 
     handler.queue().skip()?;
+    let c_uuid = track_data.uuid;
+
+    handler.queue().modify_queue(|q| {
+        let mut ids: Vec<usize> = q
+            .iter()
+            .enumerate()
+            .filter_map(|(i, q)| {
+                let data: Arc<TtsTrackData> = q.data();
+                (data.uuid == c_uuid).then_some(i)
+            })
+            .collect();
+
+        ids.sort_unstable_by(|a, b| b.cmp(a));
+        for i in ids {
+            q.remove(i);
+        }
+    });
+
     Ok(())
 }
