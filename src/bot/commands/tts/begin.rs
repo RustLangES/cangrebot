15ab2796317342c3452dc238dbf42f9a6c919265
Brayan-724 <bjqf0724@gmail.com>
2025-08-23 23:57:24 -0700

    feat(tts): Allow moderators to begin/end to another user

--- src/bot/commands/tts/begin.rs
+++ src/bot/commands/tts/begin.rs
@@ -1,16 +1,46 @@
-use poise::serenity_prelude::{CreateEmbed, CreateEmbedFooter};
+use poise::serenity_prelude::{Color, CreateEmbed, CreateEmbedFooter, UserId};
 use poise::CreateReply;
 
 use crate::bot;
 use crate::bot::commands::TtsStateExt;
 
 #[poise::command(slash_command, prefix_command, guild_only)]
-pub async fn begin(ctx: bot::Context<'_>) -> Result<(), bot::Error> {
+pub async fn begin(ctx: bot::Context<'_>, user: Option<UserId>) -> Result<(), bot::Error> {
     if ctx.data().tts.check_same_channel(&ctx).await? {
         return Ok(());
     };
 
-    ctx.data().tts.begin(ctx.author().id).await;
+    let user_id = if let Some(user) = user {
+        let guild_channel = ctx.guild_channel().await.ok_or("Not a guild channel")?;
+
+        let member = ctx.author_member().await.ok_or("Not a guild member")?;
+
+        let perms = ctx
+            .guild()
+            .ok_or("Not in a guild")?
+            .user_permissions_in(&guild_channel, member.as_ref());
+
+        if !perms.manage_channels() {
+            ctx.send(
+                CreateReply::default().reply(true).embed(
+                    CreateEmbed::default()
+                        .color(Color::RED)
+                        .title("TTS denegado")
+                        .description(
+                            "La activación externa de TTS mode es exclusivo para moderadores",
+                        ),
+                ),
+            )
+            .await?;
+            return Ok(());
+        }
+
+        user
+    } else {
+        ctx.author().id
+    };
+
+    ctx.data().tts.begin(user_id).await;
 
     ctx.send(
         CreateReply::default().reply(true).embed(
--- src/bot/commands/tts/end.rs
+++ src/bot/commands/tts/end.rs
@@ -1,16 +1,46 @@
-use poise::serenity_prelude::{Color, CreateEmbed};
+use poise::serenity_prelude::{Color, CreateEmbed, UserId};
 use poise::CreateReply;
 
 use crate::bot;
 use crate::bot::commands::TtsStateExt;
 
 #[poise::command(slash_command, prefix_command, guild_only)]
-pub async fn end(ctx: bot::Context<'_>) -> Result<(), bot::Error> {
+pub async fn end(ctx: bot::Context<'_>, user: Option<UserId>) -> Result<(), bot::Error> {
     if ctx.data().tts.check_same_channel(&ctx).await? {
         return Ok(());
     };
 
-    if !ctx.data().tts.is_active_user(&ctx.author().id).await {
+    let user_id = if let Some(user) = user {
+        let guild_channel = ctx.guild_channel().await.ok_or("Not a guild channel")?;
+
+        let member = ctx.author_member().await.ok_or("Not a guild member")?;
+
+        let perms = ctx
+            .guild()
+            .ok_or("Not in a guild")?
+            .user_permissions_in(&guild_channel, member.as_ref());
+
+        if !perms.manage_channels() {
+            ctx.send(
+                CreateReply::default().reply(true).embed(
+                    CreateEmbed::default()
+                        .color(Color::RED)
+                        .title("TTS denegado")
+                        .description(
+                            "La desactivación externa de TTS mode es exclusivo para moderadores",
+                        ),
+                ),
+            )
+            .await?;
+            return Ok(());
+        }
+
+        user
+    } else {
+        ctx.author().id
+    };
+
+    if !ctx.data().tts.is_active_user(&user_id).await {
         ctx.send(
             CreateReply::default().reply(true).embed(
                 CreateEmbed::default()
@@ -23,7 +53,7 @@ pub async fn end(ctx: bot::Context<'_>) -> Result<(), bot::Error> {
         return Ok(());
     }
 
-    ctx.data().tts.end(&ctx.author().id).await;
+    ctx.data().tts.end(&user_id).await;
 
     ctx.send(
         CreateReply::default()
