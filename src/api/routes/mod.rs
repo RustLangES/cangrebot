84da818b33f8908b18c9ad6a1e3eb7c4cb222001
DrewSC13 <drew_13_@outlook.com>
2026-06-03 12:42:11 -0400

    feat(api): add showcase endpoint

--- src/api/routes/mod.rs
+++ src/api/routes/mod.rs
@@ -2,8 +2,12 @@ mod daily_challenge;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 pub use daily_challenge::daily_challenge;
+
 mod send_message;
 pub use send_message::send_message;
+
+mod showcase;
+pub use showcase::showcase;
 
 pub mod send_stats;
 
